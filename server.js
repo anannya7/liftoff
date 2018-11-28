067fa0d7407b4ea4389378e7eac5f0128b38aa89
anannya <anannya@inventblue.com>
2018-11-28 07:49:18 +0530

    app name chANGE

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/liftoff-quiz-app'));
+app.use(express.static(__dirname + '/dist/questionnaire'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/liftoff-quiz-app/index.html'));
+res.sendFile(path.join(__dirname+'/dist/questionnaire'));
 });
 
 // Start the app by listening on the default Heroku port
